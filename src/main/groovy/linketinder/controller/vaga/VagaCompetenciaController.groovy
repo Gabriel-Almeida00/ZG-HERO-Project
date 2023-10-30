10c042fa35233f422b7703bb93818f2231f9c663
Gabriel <gaamdal557@gmail.com>
2023-10-30 01:11:44 -0300

    feat: crud em VagaCompetenciaController e VagaController

--- src/main/groovy/linketinder/controller/vaga/VagaCompetenciaController.groovy
+++ src/main/groovy/linketinder/controller/vaga/VagaCompetenciaController.groovy
@@ -1,30 +1,90 @@
 package linketinder.controller.vaga
 
+import com.google.gson.Gson
+import linketinder.dao.vaga.VagaCompetenciaDao
+import linketinder.dao.vaga.VagaDao
+import linketinder.db.ConfigDatabase
+import linketinder.db.IDatabaseConnection
+import linketinder.db.factory.DatabaseFactory
+import linketinder.db.factory.IDatabaseConnectionFactory
 import linketinder.model.VagaCompetencia
 import linketinder.model.dto.CompetenciaDTO
-import linketinder.service.vaga.IVagaCompetenciaService
+import linketinder.service.vaga.VagaCompetenciaService
+import linketinder.utils.servlet.ServletResponseUtils
+import linketinder.utils.servlet.ServletUtils
 
-class VagaCompetenciaController {
-    private IVagaCompetenciaService vagaCompetenciaService
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
-    VagaCompetenciaController(IVagaCompetenciaService vagaCompetenciaService) {
-        this.vagaCompetenciaService = vagaCompetenciaService
+@WebServlet(name = "VagaCompetenciaController", urlPatterns = "/vagaCompetencia/*")
+class VagaCompetenciaController extends HttpServlet {
+    private Gson gson = new Gson()
+    private ServletUtils servletUtils = new ServletUtils()
+    private ServletResponseUtils servletResponseUtils = new ServletResponseUtils()
+
+    ConfigDatabase configDatabase = new ConfigDatabase()
+    DatabaseFactory databaseFactory = new DatabaseFactory()
+    IDatabaseConnectionFactory factory = databaseFactory.createConnectionFactory(configDatabase)
+    IDatabaseConnection connection = factory.createConnection()
+    VagaDao vagaDao = new VagaDao(connection)
+    VagaCompetenciaDao vagaCompetenciaDao = new VagaCompetenciaDao(connection, vagaDao)
+    VagaCompetenciaService vagaCompetenciaService = new VagaCompetenciaService(vagaCompetenciaDao)
+
+    VagaCompetenciaController() {}
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            int idCompetencia = this.servletUtils.pegarIdDaUrl(request)
+            List<CompetenciaDTO> competencia = this.vagaCompetenciaService.listarCompetenciasPorVaga(idCompetencia)
+            String json = gson.toJson(competencia)
+            servletResponseUtils.writeResponse(response, json)
+
+        } catch (Exception e) {
+            this.servletResponseUtils.writeErrorResponse(response, HttpServletResponse.SC_BAD_REQUEST, e.getMessage())
+        }
     }
 
-    void adicionarVagaCompetencia(VagaCompetencia vagaCompetencia) {
-        vagaCompetenciaService.adicionarVagaCompetencia(vagaCompetencia)
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            VagaCompetencia competencia = this.servletUtils.parseObjectFromRequest(request, VagaCompetencia.class)
+            this.vagaCompetenciaService.adicionarVagaCompetencia(competencia)
+
+            servletResponseUtils.configureResponse(response)
+            response.setStatus(HttpServletResponse.SC_CREATED)
+        } catch (Exception e) {
+            this.servletResponseUtils.writeErrorResponse(response, HttpServletResponse.SC_BAD_REQUEST, e.getMessage())
+        }
     }
 
-    void atualizarNivelVagaCompetencia(VagaCompetencia vagaCompetencia) {
-        vagaCompetenciaService.atualizarNivelVagaCompetencia(vagaCompetencia)
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            int competenciaId = this.servletUtils.pegarIdDaUrl(request)
+            VagaCompetencia competencia = this.servletUtils.parseObjectFromRequest(request, VagaCompetencia.class)
+
+            competencia.setId(competenciaId)
+            this.vagaCompetenciaService.atualizarNivelVagaCompetencia(competencia)
+
+            this.servletResponseUtils.configureResponse(response)
+            response.setStatus(HttpServletResponse.SC_OK)
+        } catch (Exception e) {
+            this.servletResponseUtils.writeErrorResponse(response, HttpServletResponse.SC_BAD_REQUEST, e.getMessage())
+        }
     }
 
-    void excluirVagaCompetencia(Integer idVagaCompetencia) {
-        vagaCompetenciaService.excluirVagaCompetencia(idVagaCompetencia)
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            int idCompetencia = servletUtils.pegarIdDaUrl(request)
+            this.vagaCompetenciaService.excluirVagaCompetencia(idCompetencia)
 
-    }
-
-    List<CompetenciaDTO> listarCompetenciasPorVaga(Integer idVaga) {
-        return vagaCompetenciaService.listarCompetenciasPorVaga(idVaga)
+            response.setStatus(HttpServletResponse.SC_NO_CONTENT)
+        } catch (Exception e) {
+            this.servletResponseUtils.writeErrorResponse(response, HttpServletResponse.SC_BAD_REQUEST, e.getMessage())
+        }
     }
 }
--- src/main/groovy/linketinder/controller/vaga/VagaController.groovy
+++ src/main/groovy/linketinder/controller/vaga/VagaController.groovy
@@ -1,35 +1,94 @@
 package linketinder.controller.vaga
 
+import com.google.gson.Gson
+import linketinder.dao.vaga.VagaDao
+import linketinder.db.ConfigDatabase
+import linketinder.db.IDatabaseConnection
+import linketinder.db.factory.DatabaseFactory
+import linketinder.db.factory.IDatabaseConnectionFactory
 import linketinder.model.Vaga
 import linketinder.model.dto.VagaDTO
-import linketinder.service.vaga.IVagaService
+import linketinder.service.vaga.VagaService
+import linketinder.utils.servlet.ServletResponseUtils
+import linketinder.utils.servlet.ServletUtils
 
-import java.sql.SQLException
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
-class VagaController {
-    private IVagaService vagaService
+@WebServlet(name = "VagaController", urlPatterns = "/vaga/*")
+class VagaController extends HttpServlet {
+    private Gson gson = new Gson()
+    private ServletUtils servletUtils = new ServletUtils()
+    private ServletResponseUtils servletResponseUtils = new ServletResponseUtils()
 
-    VagaController(IVagaService vagaService) {
-        this.vagaService = vagaService
+    ConfigDatabase configDatabase = new ConfigDatabase()
+    DatabaseFactory databaseFactory = new DatabaseFactory()
+    IDatabaseConnectionFactory factory = databaseFactory.createConnectionFactory(configDatabase)
+    IDatabaseConnection connection = factory.createConnection()
+    VagaDao vagaDao = new VagaDao(connection)
+    VagaService vagaService = new VagaService(vagaDao)
+
+    VagaController() {}
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            String pathInfo = request.getPathInfo()
+            if (pathInfo == null ) {
+                List<VagaDTO> vagas = this.vagaService.listarTodasVagas()
+                String json = this.gson.toJson(vagas)
+                this.servletResponseUtils.writeResponse(response, json)
+            } else {
+                int idEmpresa = this.servletUtils.pegarIdDaUrl(request)
+                List<VagaDTO> vagas = this.vagaService.listarVagasDaEmpresa(idEmpresa)
+                String json = gson.toJson(vagas)
+                servletResponseUtils.writeResponse(response, json)
+            }
+        } catch (Exception e) {
+            this.servletResponseUtils.writeErrorResponse(response, HttpServletResponse.SC_BAD_REQUEST, e.getMessage())
+        }
     }
 
-    List<VagaDTO> listarTodasVagas() {
-        return vagaService.listarTodasVagas()
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            Vaga vaga = this.servletUtils.parseObjectFromRequest(request, Vaga.class)
+            this. vagaService.adicionarVaga(vaga)
+
+            servletResponseUtils.configureResponse(response)
+            response.setStatus(HttpServletResponse.SC_CREATED)
+        } catch (Exception e) {
+            this.servletResponseUtils.writeErrorResponse(response, HttpServletResponse.SC_BAD_REQUEST, e.getMessage())
+        }
     }
 
-    List<VagaDTO> listarVagasDaEmpresa(Integer idEmpresa) {
-        return vagaService.listarVagasDaEmpresa(idEmpresa)
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            int idVaga = this.servletUtils.pegarIdDaUrl(request)
+            Vaga vaga = this.servletUtils.parseObjectFromRequest(request, Vaga.class)
+
+            vaga.setId(idVaga)
+            this. vagaService.atualizarVaga(vaga)
+
+            this.servletResponseUtils.configureResponse(response)
+            response.setStatus(HttpServletResponse.SC_OK)
+        } catch (Exception e) {
+            this.servletResponseUtils.writeErrorResponse(response, HttpServletResponse.SC_BAD_REQUEST, e.getMessage())
+        }
     }
 
-    void adicionarVaga(Vaga vaga) {
-        vagaService.adicionarVaga(vaga)
-    }
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            int idVaga = this.servletUtils.pegarIdDaUrl(request)
+            this.vagaService.excluirVaga(idVaga)
 
-    void atualizarVaga(Vaga vaga) {
-        vagaService.atualizarVaga(vaga)
-    }
-
-    void excluirVaga(Integer idVaga) {
-        vagaService.excluirVaga(idVaga)
+            response.setStatus(HttpServletResponse.SC_NO_CONTENT)
+        } catch (Exception e) {
+            this.servletResponseUtils.writeErrorResponse(response, HttpServletResponse.SC_BAD_REQUEST, e.getMessage())
+        }
     }
 }
