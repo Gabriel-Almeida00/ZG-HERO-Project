fe88792b96545477fbf7d4c5ca9267661d52389e
Gabriel <gaamdal557@gmail.com>
2023-11-01 14:13:38 -0300

    feat: habilitando cors na API

--- src/main/groovy/linketinder/servlet/ServletResponse.groovy
+++ src/main/groovy/linketinder/servlet/ServletResponse.groovy
@@ -8,7 +8,7 @@ class ServletResponse {
     Gson gson = new Gson()
 
     void writeResponse(HttpServletResponse response){
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8080/")
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8080")
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
